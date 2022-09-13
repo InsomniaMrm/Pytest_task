f633e5695b2ba226616b8dcf297a251ce4a2b61e
mariam <maryam@sosiv.io>
2022-09-13 15:10:29 +0300

    add tests for string

---
+++ tests_string.py
@@ -0,0 +1,24 @@
+import pytest
+
+
+def lower_case(x):
+    return x.lower()
+
+
+def contains(x, y):
+    return y in x
+
+
+def test_lower_case():
+    assert lower_case('DRAGON') == 'dragon'
+
+
+def test_raises_exception_on_non_string_arguments():
+    with pytest.raises(AttributeError):
+        lower_case(9)
+
+
+@pytest.mark.parametrize("x, y, expected", [("DRAGONS", "GON", True), ("HOME", "AA", False)])
+# @pytest.mark.parametrize("y", ["GON", "AA"])
+def test_contains(x, y, expected):
+    assert contains(x, y) == expected
